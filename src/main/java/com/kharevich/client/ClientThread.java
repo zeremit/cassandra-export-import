4ad23662fdb1af99ab260c123ad956e05c9723ea
dmitry.khorevich <dmitry.khorevich@altoros.com>
2016-01-20 18:31:04 +0300

    add multithread class

---
+++ src/main/java/com/kharevich/client/ClientThread.java
@@ -0,0 +1,18 @@
+package com.kharevich.client;
+
+import com.kharevich.parser.Parser;
+
+/**
+ * Created by khorevich on 1/20/16.
+ */
+public class ClientThread extends Thread{
+
+    public ClientThread(Parser parser){
+
+    }
+
+    @Override
+    public void run() {
+
+    }
+}
